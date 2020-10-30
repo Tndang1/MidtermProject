db154d00a3758125b319d13e6639c842d17ad878
Jourdan Rentschler <jrentschler3@gmail.com>
2020-10-30 13:45:37 -0600

    Added missing field Wine entity class.

--- JPAWineNot/src/main/java/com/skilldistillery/winenot/entities/Wine.java
+++ JPAWineNot/src/main/java/com/skilldistillery/winenot/entities/Wine.java
@@ -12,6 +12,9 @@ public class Wine {
 	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(name = "label_name")
+	private String labelName;
+	
 	private String vineyard;
 	@Column(name = "vintage_year")
 	private Integer vintageYear;
@@ -30,11 +33,15 @@ public class Wine {
 		super();
 	}
 
-	public Wine(int id, String vineyeard, Integer vintageYear, String flavor, String dietary, String description,
-			String image, String pairs) {
+
+
+	
+	public Wine(int id, String labelName, String vineyard, Integer vintageYear, String flavor, String dietary,
+			String description, String image, String pairs) {
 		super();
 		this.id = id;
-		this.vineyard = vineyeard;
+		this.labelName = labelName;
+		this.vineyard = vineyard;
 		this.vintageYear = vintageYear;
 		this.flavor = flavor;
 		this.dietary = dietary;
@@ -43,77 +50,148 @@ public class Wine {
 		this.pairs = pairs;
 	}
 
-	
+
+
+
+
+
 	public int getId() {
 		return id;
 	}
+
+
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	public String getVineyeard() {
+
+
+
+	public String getLabelName() {
+		return labelName;
+	}
+
+
+
+
+	public void setLabelName(String labelName) {
+		this.labelName = labelName;
+	}
+
+
+
+
+	public String getVineyard() {
 		return vineyard;
 	}
 
-	public void setVineyeard(String vineyeard) {
-		this.vineyard = vineyeard;
+
+
+
+	public void setVineyard(String vineyard) {
+		this.vineyard = vineyard;
 	}
+
+
+
 
 	public Integer getVintageYear() {
 		return vintageYear;
 	}
 
+
+
+
 	public void setVintageYear(Integer vintageYear) {
 		this.vintageYear = vintageYear;
 	}
+
+
+
 
 	public String getFlavor() {
 		return flavor;
 	}
 
+
+
+
 	public void setFlavor(String flavor) {
 		this.flavor = flavor;
 	}
+
+
+
 
 	public String getDietary() {
 		return dietary;
 	}
 
+
+
+
 	public void setDietary(String dietary) {
 		this.dietary = dietary;
 	}
+
+
+
 
 	public String getDescription() {
 		return description;
 	}
 
+
+
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+
+
 
 	public String getImage() {
 		return image;
 	}
 
+
+
+
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+
+
 
 	public String getPairs() {
 		return pairs;
 	}
 
+
+
+
 	public void setPairs(String pairs) {
 		this.pairs = pairs;
 	}
 
+
+
+
+
+
 	@Override
 	public String toString() {
-		return "Wine [id=" + id + ", vineyard=" + vineyard + ", vintageYear=" + vintageYear + ", flavor=" + flavor
-				+ ", dietary=" + dietary + ", description=" + description + ", image=" + image + ", pairs=" + pairs
-				+ "]";
+		return "Wine [id=" + id + ", labelName=" + labelName + ", vineyard=" + vineyard + ", vintageYear=" + vintageYear
+				+ ", flavor=" + flavor + ", dietary=" + dietary + ", description=" + description + ", image=" + image
+				+ ", pairs=" + pairs + "]";
 	}
+
+
+
 
 	@Override
 	public int hashCode() {
