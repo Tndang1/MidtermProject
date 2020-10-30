74164c7c108252f3d9d2000592022e5c9e671563
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-10-30 15:17:57 -0500

    added customer entity/junit. Still needs payment and address mapped

---
+++ JPAWineNot/src/main/java/com/skilldistillery/winenot/entities/Customer.java
@@ -0,0 +1,100 @@
+package com.skilldistillery.winenot.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name="first_name")
+	private String fName;
+	@Column(name="last_name")
+	private String lName;
+	private LocalDateTime birthdate;
+	@Column(name="create_date")
+	private LocalDateTime createDate;
+	@Column(name="customer_image")
+	private String customerImage;
+	private String bio;
+	@OneToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public LocalDateTime getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(LocalDateTime birthdate) {
+		this.birthdate = birthdate;
+	}
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+	public String getCustomerImage() {
+		return customerImage;
+	}
+	public void setCustomerImage(String customerImage) {
+		this.customerImage = customerImage;
+	}
+	public String getBio() {
+		return bio;
+	}
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
