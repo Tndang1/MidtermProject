1792451abf97fade64be1917221068d771986ece
Tevon Patterson <Tevonpatt@gmail.com>
2020-10-31 18:33:03 -0700

    created customer order controller

--- WineNot/src/main/java/com/skilldistillery/winenot/controllers/OrderController.java
+++
@@ -1,5 +0,0 @@
-package com.skilldistillery.winenot.controllers;
-
-public class OrderController {
-
-}
--- WineNot/src/main/java/com/skilldistillery/winenot/data/CustomerOrderDAO.java
+++ WineNot/src/main/java/com/skilldistillery/winenot/data/CustomerOrderDAO.java
@@ -7,18 +7,17 @@ import com.skilldistillery.winenot.entities.Wine;
 
 public interface CustomerOrderDAO {
 
-	CustomerOrder findById(int id);
+	CustomerOrder findById(int id); 
 	
-	public CustomerOrder create(CustomerOrder order);
+	public CustomerOrder create(CustomerOrder order); 
 	
-	public CustomerOrder update(int id, CustomerOrder order);
+	public CustomerOrder update(int id, CustomerOrder order); 
 	
-	public boolean deleteCustomerOrder(int id);
+	public boolean deleteCustomerOrder(int id); 
 	
-	List<CustomerOrder> findAll();
+	List<CustomerOrder> findAll(); 
 	
-	public Wine addWineToOrder(int id, Wine wine);
+	public Wine addWineToOrder(int id, Wine wine); 
 	
-	public boolean removeWineFromOrder(int id, Wine wine);
-	
+	public boolean removeWineFromOrder(int id, Wine wine); 
 }
